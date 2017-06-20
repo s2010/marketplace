497e61acb46dc8b41a6d1388a65317d73ba022b9
s2010 <bashayer.hijji@gmail.com>
2017-06-20 23:08:37 +0300

    first commit

---
+++ resources/views/layouts/partials/_nav.blade.php
@@ -0,0 +1,35 @@
+<nav class="nav">
+    <div class="container">
+        <div class="nav-left">
+            <a href="{{ route('home') }}" class="nav-item is-brand">
+                {{ config('app.name') }}
+            </a>
+        </div>
+
+        <div class="nav-right nav-menu">
+            @if(auth()->check())
+                <a href="{{ route('login') }}" class="nav-item">
+                    Your account
+                </a>
+                <a href="{{ route('login') }}" class="nav-item"
+                onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                    Sign out
+                </a>
+            @else    
+            <a href="{{ route('login') }}" class="nav-item">
+                Sign in
+            </a>
+            <div class="nav-item">
+                 <a href="{{ route('register') }}" class="button">
+                   Start selling 
+                </a>
+            </div>
+            @endif
+        </div>
+    </div>
+</nav>
+
+<form id="logout" action="{{ route('logout') }}" method="POST" class="is-hidden">
+    {{csrf_field()}}
+</form>
