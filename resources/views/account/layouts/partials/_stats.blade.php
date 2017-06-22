b1e6cce459664b82de03f2bf8ea26df6bb9efa57
s2010 <bashayer.hijji@gmail.com>
2017-06-22 04:51:52 +0300

    Stats design

--- resources/views/account/layouts/partials/_stats.blade.php
+++ resources/views/account/layouts/partials/_stats.blade.php
@@ -1,7 +1,30 @@
 <section class="hero is-primary">
     <div class="hero-body">
-        <div class="level">
-         
+        <div class="level is-mobile">
+            <div class="level-item has-text-centered">
+                <div>
+                    <p class="heading">Files</p>
+                    <p class="title">5</p>
+                </div>
+            </div>
+            <div class="level-item has-text-centered">
+                <div>
+                    <p class="heading">Sales</p>
+                    <p class="title">9</p>
+                </div>
+            </div>
+            <div class="level-item has-text-centered">
+                <div>
+                    <p class="heading">Sales this month</p>
+                    <p class="title">$ 10</p>
+                </div>
+            </div>
+            <div class="level-item has-text-centered">
+                <div>
+                    <p class="heading">Lifetime sales</p>
+                    <p class="title">$ 199</p>
+                </div>
+            </div> 
         </div>
     </div>
 </section>
